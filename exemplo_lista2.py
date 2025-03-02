dc7627e3e898286ede4adb90b92c8cf842de9f69
arnnor rocha <arnnor.rocha@gmail.com>
2025-03-02 19:24:54 -0300

    Exemplo de lista e dicionário.

---
+++ exemplo_lista2.py
@@ -0,0 +1,18 @@
+lista: list = ["Sapato", 39, 10.38, True]
+
+produto_01: dict = {
+    "nome": "Sapato",
+    "quantidade": 39,
+    "preco": 10.38
+    "disponibilidade": True
+}
+
+produto_02: dict = {
+    "nome": "Televisao",
+    "quantidade" : 10,
+    "preco": 70.38
+    "disponibilidade": False
+}
+
+carrinho: list = []
+
